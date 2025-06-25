6f384d957fe514fd3b1dab5a92b0df1fb776999b
HP <pmon3140@gmail.com>
2025-06-26 00:14:17 +0530

    initiate commit

--- app.py
+++ app.py
@@ -72,4 +72,5 @@ def download_file(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # ✅ Use this version for deployment on Render
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
